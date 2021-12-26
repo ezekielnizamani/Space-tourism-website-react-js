abe16ee5f3a07294dff1b61cfca5c8ac208e5147
ezekiel <ezekielnizamani0@gmail.com>
2021-12-26 02:08:25 -0800

    starting cleanup

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+
+import Home from './pages/Home'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+const Routing = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
+    
+    )
+}
+
+export default Routing  
